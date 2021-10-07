1a738923c1e80c73f89b5afd3f3a69519b422288
Adam Girard-McFadden <adam_m_20@hotmail.com>
2021-10-07 15:34:47 -0400

    footer essentially complete

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,22 +1,26 @@
 import React from "react";
+import { FaGithub, FaEnvelope, FaLinkedin } from "react-icons/fa";
 
 function Footer() {
   return (
     <footer className="col-12">
-      <ul className="d-flex flex-row justify-content-center">
+      <ul className="d-flex flex-row justify-content-center icon-cont">
         <li>
-          <a href="#github">
-            <span>Github img</span>
+          <a className="icon-style" href="https://github.com/adamgmcfadden">
+            <FaGithub size="3em" />
           </a>
         </li>
         <li>
-          <a href="#linkedIn">
-            <span>LinkedIn img</span>
+          <a
+            className="icon-style"
+            href="https://www.linkedin.com/in/adam-mcfadden-70768b47"
+          >
+            <FaLinkedin size="3em" />
           </a>
         </li>
         <li>
-          <a href="#email">
-            <span>Email img</span>
+          <a className="icon-style" href="mailto:adam_m_20@hotmail.com">
+            <FaEnvelope size="3em" />
           </a>
         </li>
       </ul>
