bd95febae9529c62bb44091c1fa450b73734004b
Adam Girard-McFadden <adam_m_20@hotmail.com>
2021-10-08 09:50:45 -0400

    resume section complete - now start styling

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -1,10 +1,52 @@
 import React from "react";
 
-const Resume = () => (
-  <div>
-    <h1 className="title">Resume Page</h1>
-    <p>Temporary Resume Page - Maybe add PDF link to actual resume instead?</p>
-  </div>
-);
+function Resume() {
+  const frontend = [
+    "HTML",
+    "CSS",
+    "javaScript",
+    "jQuery",
+    "responsive design",
+    "React",
+    "Bootstrap",
+  ];
+
+  const backend = [
+    "APIs",
+    "Node",
+    "Express",
+    "MySQL, Sequelize",
+    "MongoDB, Mongoose",
+    "REST",
+    "GraphQL",
+  ];
+  return (
+    <div className="container-style">
+      <h1 className="page-title">Resume</h1>
+      <h3>
+        Download my&nbsp;
+        <a className="resume-link" href="#resumelink">
+          Resume
+        </a>
+      </h3>
+      <h2>
+        Front-end Proficiencies
+        <ul>
+          {frontend.map((tech) => (
+            <li key={tech}>{tech}</li>
+          ))}
+        </ul>
+      </h2>
+      <h2>
+        Back-end Proficiencies
+        <ul>
+          {backend.map((tech) => (
+            <li key={tech}>{tech}</li>
+          ))}
+        </ul>
+      </h2>
+    </div>
+  );
+}
 
 export default Resume;
