73e232f8d3a4aa84dc5a05c139243ed592723942
Adam Girard-McFadden <adam_m_20@hotmail.com>
2021-10-08 22:22:42 -0400

    pointer corrections

---
+++ src/components/Project/index.js
@@ -0,0 +1,12 @@
+// import dependencies -- React and project list
+import React from "react";
+
+function Project() {
+  return (
+    <section>
+      <ProjectList />
+    </section>
+  );
+}
+
+export default Project;
